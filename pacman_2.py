703d7b83015814c13b96a94978b589e1c4d529a6
Gabriela Pereira <55415365+GabrielaPRocha@users.noreply.github.com>
2022-03-01 16:02:44 -0300

    adicionado o fantasma

--- pacman_2.py
+++ pacman_2.py
@@ -1,4 +1,5 @@
 import pygame
+from abc import ABCMeta, abstractmethod
 
 pygame.init()
 
@@ -8,11 +9,27 @@ fonte = pygame.font.SysFont("arial", 24, True, False)
 
 AMARELO = (255, 255, 0)
 PRETO = (0, 0, 0)
-AZUL = (70,130,180)
+BRANCO =(255, 255, 255)
+AZUL = (70, 130, 180)
+VERMELHO = (255, 0, 0)
 VELOCIDADE = 1
 
 
-class Cenario:
+class ElementoJogo(metaclass=ABCMeta):
+    @abstractmethod
+    def pintar(self, tela):
+        pass
+
+    @abstractmethod
+    def calcular_regras(self):
+        pass
+
+    @abstractmethod
+    def processar_eventos(self, eventos):
+        pass
+
+
+class Cenario(ElementoJogo):
 
     def __init__(self, tamanho, pac):
         self.pacman = pac
@@ -52,8 +69,9 @@ class Cenario:
 
     def pintar_pontos(self, tela):
         pontos_x = 30 * self.tamanho
-        img_pontos = fonte.render("Score: {}".format(self.pontos), True,AMARELO)
+        img_pontos = fonte.render("Score: {}".format(self.pontos), True, AMARELO)
         tela.blit(img_pontos, (pontos_x, 50))
+
     def pintar_linha(self, tela, numero_linha, linha):
 
         for numero_coluna, coluna in enumerate(linha):
@@ -82,9 +100,13 @@ class Cenario:
                     self.pontos += 1
                     self.matriz[lin][col] = 0
 
+    def processar_eventos(self, evts):
+        for e in evts:
+            if e.type == pygame.QUIT:
+                exit()
 
-class Pacman:
 
+class Pacman(ElementoJogo):
     def __init__(self, tamanho):
         self.coluna = 1
         self.linha = 1
@@ -139,41 +161,84 @@ class Pacman:
                     self.vel_y = 0
                 elif e.key == pygame.K_DOWN:
                     self.vel_y = 0
-   # def processar_eventos_mouse(self,eventos):
-    #    delay = 100
-     #   for e in eventos:
-      #      if e.type ==pygame.MOUSEMOTION:
-       #         mouse_x,mouse_y = e.pos
-        #        self.coluna = (mouse_x - self.centro_x) / delay
-         #       self.linha  = (mouse_y - self.centro_y )/ delay
+        # def processar_eventos_mouse(self,eventos):
+        #    delay = 100
+        #   for e in eventos:
+        #      if e.type ==pygame.MOUSEMOTION:
+        #         mouse_x,mouse_y = e.pos
+            #        self.coluna = (mouse_x - self.centro_x) / delay
+                #       self.linha  = (mouse_y - self.centro_y )/ delay
 
     def aceitar_movimento(self):
         self.linha = self.linha_intecao
         self.coluna = self.coluna_intecao
 
 
+class Fantasma(ElementoJogo):
+    def __init__(self, cor, tamanho):
+        self.coluna = 6.0
+        self.linha = 8.0
+        self.tamanho = tamanho
+        self.cor = cor
+
+    def pintar(self, tela):
+        fatia = self.tamanho // 8
+        px = int(self.coluna * self.tamanho)
+        py = int(self.linha * self.tamanho)
+        contorno = [(px, py + self.tamanho),
+                    (px + fatia * 1, py + fatia * 2),
+                    (px + fatia * 2, py + fatia // 2),
+                    (px+fatia*3, py),
+                    (px+fatia*5, py),
+                    (px + fatia * 6, py + fatia // 2),
+                    (px + fatia * 7, py + fatia * 2),
+                    (px + self.tamanho, py + self.tamanho)]
+        pygame.draw.polygon(tela, self.cor, contorno, 0)
+
+        olho_raio_ext = fatia
+        olho_raio_int = fatia // 2
+
+        olho_e_x = int(px + fatia * 2.5)
+        olho_e_y = int(py + fatia * 2.5)
+
+        olho_d_x = int(px + fatia * 5.5)
+        olho_d_y = int(py + fatia * 2.5)
+        
+        pygame.draw.circle(tela, BRANCO, (olho_e_x, olho_e_y), olho_raio_ext, 0)
+        pygame.draw.circle(tela, PRETO, (olho_e_x, olho_e_y), olho_raio_int, 0)
+        pygame.draw.circle(tela, BRANCO, (olho_d_x, olho_d_y), olho_raio_ext, 0)
+        pygame.draw.circle(tela, PRETO, (olho_d_x, olho_d_y), olho_raio_int, 0)
+
+    def calcular_regras(self):
+        pass
+
+    def processar_eventos(self, evts):
+        pass
+
+
 if __name__ == "__main__":
+
     size = 600 // 30
     pacman = Pacman(size)
+    blinky = Fantasma(VERMELHO, size)
     cenario = Cenario(size, pacman)
 
     while True:
-        #CALCULAR AS REGRAS
+        # CALCULAR AS REGRAS
         pacman.calcular_regras()
         cenario.calcular_regras()
 
-        #PINTAR A TELA
+        # PINTAR A TELA
 
         screen.fill(PRETO)
         cenario.pintar(screen)
         pacman.pintar(screen)
+        blinky.pintar(screen)
         pygame.display.update()
         pygame.time.delay(100)
 
-        #CAPTURA DE EVENTOS
+        # CAPTURA DE EVENTOS
 
         eventos = pygame.event.get()
-        for e in eventos:
-            if e.type == pygame.QUIT:
-                exit()
         pacman.processar_eventos(eventos)
+        cenario.processar_eventos(eventos)
